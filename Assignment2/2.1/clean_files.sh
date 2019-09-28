56bbe7d822689a87808e3b4459420f3a2ac9efcf
Yugesh Ajit Kothari <yugesh@cse.iitk.ac.in>
2019-09-28 16:21:12 +0530

    Scripts

---
+++ Assignment2/2.1/clean_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm 0*.txt 2> /dev/null
+rm 1*.txt 2> /dev/null
+rm 2*.txt 2> /dev/null
---
+++ Assignment2/2.1/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make all
+
+for i in {1..10}
+do
+    for j in {0..2}
+    do
+        rm ${j}_time_$i.txt 2> /dev/null
+        touch ${j}_time_$i.txt
+        mpiexec -f hostfile.txt -np 30 -ppn 1 ./src.x ${j} | tee ${j}_time_$i.txt
+    done
+done
+
+make clean
